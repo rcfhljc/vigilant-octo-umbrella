5ad02c22b2bb6444d80fd352243c5226d6e44a7c
rcfhljc <nini2001@inbox.ru>
2023-06-19 18:50:03 +1000

    Чистовик

--- Program.cs
+++ Program.cs
@@ -1,37 +1,71 @@
-﻿
-//2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// ЗАДАЧА 10
+// int number = ReadInt("Введите трехзначное число: ");
+// int amount = number.ToString().Length;
+// if (amount < 3 || amount > 3)
+// {
+//     Console.WriteLine("Вы ввели не трехзначное число");
+// }
+// else
+// {
+//     Console.WriteLine(InCenter(number));
+// }
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int InCenter(int a)
+// {
 
-// Console.WriteLine("Введите первое число:");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число:");
-// int b = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+//     int result = ((a / 10) % 10);
+//     return result;
+// }
 
-//3.Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-// Console.WriteLine("Введите первое число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int b = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите третье число");
-// int c = Convert.ToInt32(Console.ReadLine());
-// int max = a;
-// if (max < b) max = b;
-// if (max < c) max = c;
-// Console.WriteLine("Максимальное из этих чисел ="+max );
 
-//6.Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
 
-// Console.WriteLine("Введите целое число");
-// int number = Convert.ToInt32(Console.ReadLine());
-// if (number % 2 == 0)
-// Console.WriteLine("Четное");
-// else Console.WriteLine("Нечетное");
+// ЗАДАЧА 13
+// int number = ReadInt("Введите число: ");
+// int count = number.ToString().Length;
+// Console.Write(MakeArray(number, count));
 
-//8.Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int MakeArray(int a, int b)
+// {
+//     int result = 0;
+//     if (b < 3)
+//     {
+//         Console.Write("Третьей цифры нет, держи: ");
+//     }
+//     else
+//     {
+//         int c = 1;
+//         for (int i = b; i > 3; i--)
+//         {
+//             c = c * 10;
+//         }
 
-// Console.WriteLine("Введите число N");
-// int n = Convert.ToInt32(Console.ReadLine());
-// for(int index= 1; index <= n; index++)
-// if (index % 2 == 0)
-// Console.Write(index + ", ");
+//         result = (a / c) % 10;
+//     }
+//     return result;
+// }
+
+// ЗАДАЧА 15
+// int Prompt(string msg)
+// {
+//     Console.WriteLine($"{msg}");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int number = Prompt("Введите число");
+// string ss = ("Это не день недели");
+// string check(int number)
+// {
+//     if (number >= 6 && number < 8) ss = ("Это выходной");
+//     if (number >= 1 && number < 7) ss = ("Это будний день");
+//     return ss;
+// }
+// Console.WriteLine(check(number));
