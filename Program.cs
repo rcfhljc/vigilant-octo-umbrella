5309c31e28852fdad7650fc7ffd38d728b5116c9
rcfhljc <nini2001@inbox.ru>
2023-06-22 23:56:06 +1000

    2 задача слишком сложная,но вроде получилось

--- Program.cs
+++ Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int len = number.Length;
+
+// if (len == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - не является палиндромом");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"{number} - не является пятизначным числом");
+// }
+
+
+// int a1 = ReadInt("Введите координату A первой точки: ");
+// int b1 = ReadInt("Введите координату B первой точки: ");
+// int c1 = ReadInt("Введите координату C первой точки: ");
+// int a2 = ReadInt("Введите координату A второй точки: ");
+// int b2 = ReadInt("Введите координату B второй точки: ");
+// int c2 = ReadInt("Введите координату C второй точки: ");
+
+// int q = a2 - a1;
+// int w = b2 - b1;
+// int e = c1 - c2;
+
+// double length = Math.Sqrt(q * q + w * w + e * e);
+// Console.WriteLine($"Длинна отрезка {length}");
+
+// int ReadInt(string message)
+// {
+//  Console.Write(message);
+//  return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+
+// int number = ReadInt("Введите число N: ");
+
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.Write($"{i*i*i} ");
+// }
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
