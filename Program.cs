f978df2995ef84a91f4f3bac4a522bd81a4a456b
rcfhljc <nini2001@inbox.ru>
2023-06-26 18:12:04 +1000

    Весь день сидел и тупил над 3 задачей

--- Program.cs
+++ Program.cs
@@ -1,55 +1,40 @@
-﻿// Console.WriteLine("Введите число: ");
-// string number = Console.ReadLine();
-// int len = number.Length;
+﻿// Console.WriteLine("введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int step = a;
 
-// if (len == 5)
+// for (int i = 1; i < b; i++)
 // {
-//     if (number[0] == number[4] && number[1] == number[3])
-//     {
-//         Console.WriteLine($"{number} - Палиндром");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"{number} - не является палиндромом");
-//     }
+// step = step * a;
 // }
-// else
+// Console.WriteLine("A в степени B равно: " + step);
+
+
+// Console.WriteLine("введите число");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while (i > 0)
 // {
-//     Console.WriteLine($"{number} - не является пятизначным числом");
+// int num = i % 10;
+// i = i / 10;
+// sum = sum + num;
 // }
+// Console.WriteLine("сумма всех цифр в числе равна: " + sum);
 
 
-// int a1 = ReadInt("Введите координату A первой точки: ");
-// int b1 = ReadInt("Введите координату B первой точки: ");
-// int c1 = ReadInt("Введите координату C первой точки: ");
-// int a2 = ReadInt("Введите координату A второй точки: ");
-// int b2 = ReadInt("Введите координату B второй точки: ");
-// int c2 = ReadInt("Введите координату C второй точки: ");
+// int [] numbers = new int[8];
+// Console.Write("[");
 
-// int q = a2 - a1;
-// int w = b2 - b1;
-// int e = c1 - c2;
+// for (int i = 0; i < numbers.Length; i++)
+//  {
+//  numbers [i] = new Random().Next(0, 11);
+//  Console.Write(" " + Method (i) + " ");
+//  }
+// Console.Write("]");
 
-// double length = Math.Sqrt(q * q + w * w + e * e);
-// Console.WriteLine($"Длинна отрезка {length}");
-
-// int ReadInt(string message)
+// int Method (int a)
 // {
-//  Console.Write(message);
-//  return Convert.ToInt32(Console.ReadLine());
-// }
-
-
-
-
-// int number = ReadInt("Введите число N: ");
-
-// for (int i = 1; i <= number; i++)
-// { 
-//     Console.Write($"{i*i*i} ");
-// }
-// int ReadInt(string message)
-// {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
+//  return numbers[a];
 // }
